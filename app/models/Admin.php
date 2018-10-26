1d8442efa6d4e14b34f4b57fd2cf6fa440d04a92
arisyaagustiana <putuarisyaa@gmail.com>
2018-10-26 19:36:54 +0800

    sudah

---
+++ app/models/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+class Admin {
+  static function login(){
+    $email    = Input::post('email');
+    $password = Input::post('password');
+
+    $sql = "SELECT email, password FROM admin_new WHERE email = ? AND password = ?";
+
+    $prep = DB::conn()->prepare($prep);
+
+    $prep->execute([
+        $email,
+        $password
+    ]);
+
+    if($prep->rowCont() > 0){
+      $data = $prep->fetch(PDO::FETCH_OBJ);
+
+      Session::sess('id_admin', $data->id_admin);
+
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
